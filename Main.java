2612fb5ebf8bc2b4548bc4885483af4c477db3df
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2021-01-29 17:29:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; // This is really important for you to use scanner without this scanner does not function properly.
+// My idea is that I will be creating a code that will making a code that will show what games a perosn can like.
+class Main {
+  public static void main(String[] args) {
+   Scanner scan = new Scanner (System.in);
+
+  }
+}
